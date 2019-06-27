73b6702f5d58641c651a3c3cb3126b98fb588122
BurnySc2 <gamingburny@gmail.com>
2019-06-27 13:49:39 +0200

    Add init file so that pip install command also copies the dicts folder

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         "License :: OSI Approved :: MIT License",
 
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ]
 )
