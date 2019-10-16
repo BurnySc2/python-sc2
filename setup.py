48b42752718024eaaf038a3e2ff3eb97d33d95b7
BuRny <gamingburny@gmail.com>
2019-10-16 16:11:48 +0200

    Update docs and some introduction/tutorial text, bump version

--- setup.py
+++ setup.py
@@ -4,24 +4,23 @@ from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
 
 pfile = Project(chdir=False).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+requirements = convert_deps_to_pip(pfile["packages"], r=False)
+test_requirements = convert_deps_to_pip(pfile["dev-packages"], r=False)
 
 setup(
-    name = "burnysc2",
-    packages = find_packages(),
-    version = "0.12.3",
-    description = "A StarCraft II API Client for Python 3",
+    name="burnysc2",
+    packages=find_packages(),
+    version="0.12.4",
+    description="A StarCraft II API Client for Python 3",
     license="MIT",
-    author = "BurnySc2",
-    author_email = "gamingburny@gmail.com",
-    url = "https://github.com/Burnysc2/python-sc2",
-    keywords = ["StarCraft", "StarCraft 2", "StarCraft II", "AI", "Bot"],
+    author="BurnySc2",
+    author_email="gamingburny@gmail.com",
+    url="https://github.com/Burnysc2/python-sc2",
+    keywords=["StarCraft", "StarCraft 2", "StarCraft II", "AI", "Bot"],
     setup_requires=["pipenv"],
     install_requires=requirements,
-    classifiers = [
+    classifiers=[
         "Development Status :: 3 - Alpha",
-
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
@@ -29,10 +28,8 @@ setup(
         "Topic :: Games/Entertainment :: Real Time Strategy",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-
         "License :: OSI Approved :: MIT License",
-
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-    ]
+    ],
 )
