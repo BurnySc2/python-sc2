076fe3f03d921c307637c0f3640105ddd8db065c
BuRny <gamingburny@gmail.com>
2019-11-23 20:13:38 +0100

    Revert unit_command.py

--- sc2/unit_command.py
+++ sc2/unit_command.py
@@ -32,7 +32,7 @@ class UnitCommand:
 
     @property
     def combining_tuple(self):
-        return (self.ability, self.target, self.ability in COMBINEABLE_ABILITIES, self.queue)
+        return (self.ability, self.target, self.queue, self.ability in COMBINEABLE_ABILITIES)
 
     def __repr__(self):
         return f"UnitCommand({self.ability}, {self.unit}, {self.target}, {self.queue})"
