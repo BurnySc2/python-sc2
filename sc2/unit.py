0805494994c1d8ff83d291708b1e66ef9401ccf0
BurnySc2 <gamingburny@gmail.com>
2019-12-11 08:00:05 +0000

    Deploying to gh-pages from develop 950e04c7fd202859400b6ef260f9da1c16092c43

--- sc2/unit.py
+++ sc2/unit.py
@@ -59,8 +59,6 @@ from .ids.unit_typeid import UnitTypeId
 from .position import Point2, Point3
 from .unit_command import UnitCommand
 
-warnings.simplefilter("once")
-
 if TYPE_CHECKING:
     from .bot_ai import BotAI
     from .game_data import AbilityData, UnitTypeData
@@ -1118,7 +1116,9 @@ class Unit:
         :param queue:
         """
         if unit in {UnitTypeId.EXTRACTOR, UnitTypeId.ASSIMILATOR, UnitTypeId.REFINERY}:
-            assert isinstance(position, Unit), f"When building the gas structure, the target needs to be a unit (the vespene geysir) not the position of the vespene geysir."
+            assert isinstance(
+                position, Unit
+            ), f"When building the gas structure, the target needs to be a unit (the vespene geysir) not the position of the vespene geysir."
         return self(self._bot_object._game_data.units[unit.value].creation_ability.id, target=position, queue=queue)
 
     def build_gas(self, target_geysir: Unit, queue: bool = False) -> UnitCommand:
@@ -1132,7 +1132,9 @@ class Unit:
         :param queue:
         """
         gas_structure_type_id: UnitTypeId = race_gas[self._bot_object.race]
-        assert isinstance(target_geysir, Unit), f"When building the gas structure, the target needs to be a unit (the vespene geysir) not the position of the vespene geysir."
+        assert isinstance(
+            target_geysir, Unit
+        ), f"When building the gas structure, the target needs to be a unit (the vespene geysir) not the position of the vespene geysir."
         return self(
             self._bot_object._game_data.units[gas_structure_type_id.value].creation_ability.id,
             target=target_geysir,
