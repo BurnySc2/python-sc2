329359e9ed5201b83fd8fa94e6df58a9763babf0
BurnySc2 <gamingburny@gmail.com>
2021-02-17 18:35:22 +0000

    Deploying to gh-pages from develop @ c24bd79922d11f43ba0a075f65934b6e44459b75 🚀

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ test_requirements = convert_deps_to_pip(pfile["dev-packages"], r=False)
 setup(
     name="burnysc2",
     packages=find_packages(exclude=["examples*", "examples"]),
-    version="5.0.6",
+    version="5.0.7",
     description="A StarCraft II API Client for Python 3",
     license="MIT",
     author="BurnySc2",
