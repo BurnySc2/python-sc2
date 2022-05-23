6e85da4a5b902cfa8c925f4dd51da43918dc7349
BurnySc2 <gamingburny@gmail.com>
2022-05-23 20:20:17 +0000

    Deploying to gh-pages from develop @ b7b617f1539bf8808e8db844a8a5e9ff0b6d4243 🚀

--- docs_generate/conf.py
+++ docs_generate/conf.py
@@ -26,7 +26,11 @@ author = "tweakimp, BurnySc2"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
