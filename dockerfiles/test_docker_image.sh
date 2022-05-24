d961207eadcac7ac487532dd9f335bd9bfc9837e
BurnySc2 <gamingburny@gmail.com>
2022-05-24 16:53:51 +0000

    Deploying to gh-pages from develop @ 3586c65c410b7a3876980e22674ca9d3d032465e 🚀

--- dockerfiles/test_docker_image.sh
+++ dockerfiles/test_docker_image.sh
@@ -1,12 +1,30 @@
 # This script is meant for development, which produces fresh images and then runs tests
+# Run via
+# sh dockerfiles/test_docker_image.sh
 
 # Set which versions to use
 export VERSION_NUMBER=${VERSION_NUMBER:-1.0.0}
 export PYTHON_VERSION=${PYTHON_VERSION:-3.10}
 export SC2_VERSION=${SC2_VERSION:-4.10}
 
-# Build images
-docker build -f dockerfiles/Dockerfile -t burnysc2/python-sc2-docker:latest --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION .
+# For better readability, set local variables
+IMAGE_NAME=burnysc2/python-sc2-docker:local
+BUILD_ARGS=--build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg SC2_VERSION=$SC2_VERSION
+
+# Allow image squashing by enabling experimental docker features
+# https://stackoverflow.com/a/21164441/10882657
+# https://github.com/actions/virtual-environments/issues/368#issuecomment-582387669
+file=/etc/docker/daemon.json
+if [ ! -e "$file" ]; then
+  echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json
+  sudo systemctl restart docker.service
+fi
+
+# Build image without context
+# https://stackoverflow.com/a/54666214/10882657
+docker build -t $IMAGE_NAME $BUILD_ARGS - < dockerfiles/Dockerfile
+# Build squashed image where the layers are combined to one
+#docker build -t $IMAGE_NAME-squashed --squash $BUILD_ARGS - < dockerfiles/Dockerfile
 
 # Delete previous container if it exists
 docker rm -f test_container
@@ -17,7 +35,7 @@ docker run -i -d \
   --name test_container \
   --mount type=bind,source="$(pwd)",destination=/root/python-sc2,readonly \
   --entrypoint /bin/bash \
-  burnysc2/python-sc2-docker:latest
+  $IMAGE_NAME
 
 # Install python-sc2, via mount the python-sc2 folder will be available
 docker exec -i test_container bash -c "pip install poetry \
