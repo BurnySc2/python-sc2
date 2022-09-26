d763e4028f71a8ac30a5bca4b83a3da8a118aa07
burny <gamingburny@gmail.com>
2022-09-26 20:59:35 +0200

    Fix damage-test bot and queries-test bot

--- test/damagetest_bot.py
+++ test/damagetest_bot.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import math
 
 from loguru import logger
--- test/queries_test_bot.py
+++ test/queries_test_bot.py
@@ -4,7 +4,10 @@ These query functions are:
 self.can_place (RequestQueryBuildingPlacement)
 TODO: self.client.query_pathing (RequestQueryPathing)
 """
+import os
 import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from typing import List, Union
 
 from loguru import logger
