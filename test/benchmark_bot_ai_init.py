09510a7469403dfd882e40fcb8936273eba6b72f
burny <gamingburny@gmail.com>
2022-10-01 21:19:41 +0200

    Add benchmark_bot_ai_init to ci

--- test/benchmark_bot_ai_init.py
+++ test/benchmark_bot_ai_init.py
@@ -2,20 +2,13 @@ from test.test_pickled_data import MAPS, get_map_specific_bot
 
 
 def _test_run_bot_ai_init_on_all_maps():
-    # BENCHMARK OLD: 4.4036 mean
-
-    # BENCHMARK DATACLASS with converting observation: 1.7375 - 1.7614 mean
-    # BENCHMARK DATACLASS with converting each unit from protobuf: 1.7656 - 1.8001 mean
-
-    # BENCHMARK PYDANTIC: 2.0253 mean
-
+    # Run bot initialization from pickled files via get_map_specific_bot()
     for map_ in MAPS:
         _result = get_map_specific_bot(map_)
 
 
 def test_bench_bot_ai_init(benchmark):
     _result = benchmark(_test_run_bot_ai_init_on_all_maps)
-    # assert check_result(result, correct_result)
 
 
 # Run this file using
--- test/test_pickled_data.py
+++ test/test_pickled_data.py
@@ -45,7 +45,7 @@ def get_map_specific_bot(map_path: Path) -> BotAI:
     with lzma.open(str(map_path.absolute()), "rb") as f:
         raw_game_data, raw_game_info, raw_observation = pickle.load(f)
 
-    # Build fresh bot object, and load the pickle'd data into the bot object
+    # Build fresh bot object, and load the pickled data into the bot object
     bot = BotAI()
     game_data = GameData(raw_game_data.data)
     game_info = GameInfo(raw_game_info.game_info)
