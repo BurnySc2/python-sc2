2ee53873aef1fa0bf66d816024480cd907876f34
burny <gamingburny@gmail.com>
2023-01-14 16:56:58 +0100

    Fix benchmarks

--- test/benchmark_bot_ai_init.py
+++ test/benchmark_bot_ai_init.py
@@ -1,12 +1,15 @@
 from test.test_pickled_data import MAPS, build_bot_object_from_pickle_data, load_map_pickle_data
 from typing import Any, List, Tuple
 
+import pytest
+
 
 async def _test_run_bot_ai_init_on_all_maps(pickle_data: List[Tuple[Any, Any, Any]]):
     for data in pickle_data:
         await build_bot_object_from_pickle_data(*data)
 
 
+@pytest.mark.asyncio
 async def test_bench_bot_ai_init(benchmark):
     # Load pickle files outside of benchmark
     map_pickle_data: List[Tuple[Any, Any, Any]] = [load_map_pickle_data(path) for path in MAPS]
--- test/benchmark_prepare_units.py
+++ test/benchmark_prepare_units.py
@@ -1,16 +1,19 @@
 from test.test_pickled_data import MAPS, get_map_specific_bot
 from typing import TYPE_CHECKING, List
 
+import pytest
+
 if TYPE_CHECKING:
     from sc2.bot_ai import BotAI
 
 
-def _run_prepare_units(bot_objects: List["BotAI"]):
+async def _run_prepare_units(bot_objects: List["BotAI"]):
     for bot_object in bot_objects:
-        bot_object._prepare_units()
+        await bot_object._prepare_units()
 
 
-def test_bench_prepare_units(benchmark):
+@pytest.mark.asyncio
+async def test_bench_prepare_units(benchmark):
     bot_objects = [get_map_specific_bot(map_) for map_ in MAPS]
     _result = benchmark(_run_prepare_units, bot_objects)
 
