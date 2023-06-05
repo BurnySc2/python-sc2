42feed3db6a2fa5cdc2365adaff0fb27d968a39b
Dyson Returns <4380544+dysonreturns@users.noreply.github.com>
2023-06-05 16:39:12 -0700

    Mod: Helpful alternative command in comments for proto generation. Unsure how Windows responds to the input.

--- generate_protobufs.py
+++ generate_protobufs.py
@@ -1,5 +1,8 @@
 import os
 
 if __name__ == "__main__":
-    # name each individual .proto file if *.proto doesn't work, sperated by space
     os.system('protoc --python_out=./sc2/ ./s2clientprotocol/*.proto')
+    # name each individual .proto file if *.proto doesn't work, sperated by space
+    #os.system('protoc --python_out=./sc2/ s2clientprotocol/common.proto s2clientprotocol/data.proto s2clientprotocol/debug.proto s2clientprotocol/error.proto s2clientprotocol/query.proto s2clientprotocol/raw.proto s2clientprotocol/sc2api.proto s2clientprotocol/score.proto s2clientprotocol/spatial.proto s2clientprotocol/ui.proto')
+    
+
