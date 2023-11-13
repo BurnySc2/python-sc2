fb6eb4c27ccd11d9e8c247ff87239eb2320d7a15
kabrooksRUSH <kylebrooks0125@gmail.com>
2023-11-13 04:29:43 -0500

    added test bot

---
+++ test_bot.py
@@ -0,0 +1,16 @@
+from sc2 import maps
+from sc2.player import Bot, Computer
+from sc2.main import run_game
+from sc2.data import Race, Difficulty
+from sc2.bot_ai import BotAI
+
+class WorkerRushBot(BotAI):
+    async def on_step(self, iteration: int):
+        if iteration == 0:
+            for worker in self.workers:
+                worker.attack(self.enemy_start_locations[0])
+
+run_game(maps.get("allscion"), [
+    Bot(Race.Zerg, WorkerRushBot()),
+    Computer(Race.Protoss, Difficulty.Medium)
+], realtime=True)
