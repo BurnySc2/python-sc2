b60f601cc70eb03aab64410c6c6295fc5980be25
burnysc2 <gamingburny@gmail.com>
2024-08-03 03:27:42 +0200

    Enable 'test_protobuf_implementation' again but disable for macOS

--- test/test_pickled_data.py
+++ test/test_pickled_data.py
@@ -69,12 +69,11 @@ def get_map_specific_bot(map_path: Path) -> BotAI:
     return build_bot_object_from_pickle_data(*data)
 
 
-# TODO Fixme
-# def test_protobuf_implementation():
-#     """Make sure that cpp is used as implementation"""
-#     # Doesn't seem to be implemented in newer python versions
-#     if sys.version_info.major == 3 and sys.version_info.minor < 10:
-#         assert api_implementation.Type() == "cpp"
+def test_protobuf_implementation():
+    """Make sure that cpp is used as implementation"""
+    # Doesn't seem to be implemented in newer python versions
+    if sys.version_info.major == 3 and sys.version_info.minor < 10 and sys.platform != "darwin":
+        assert api_implementation.Type() == "cpp"
 
 
 def test_bot_ai():
